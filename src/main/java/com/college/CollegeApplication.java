f5aa2be499972c65fe1958ac41396ab7b7ce1927
unknown <premprasadsaw73@gmail.com>
2020-05-13 02:25:19 +0530

    first commit

--- src/main/java/com/college/CollegeApplication.java
+++ src/main/java/com/college/CollegeApplication.java
@@ -2,9 +2,18 @@ package com.college;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+
 
 @SpringBootApplication
-public class CollegeApplication {
+public class CollegeApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CollegeApplication.class);
+    }	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CollegeApplication.class, args);
