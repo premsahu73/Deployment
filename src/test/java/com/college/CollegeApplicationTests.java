882c7517ebc9bb938bcf952546ac57e640e5f473
unknown <premprasadsaw73@gmail.com>
2020-05-12 22:01:56 +0530

    first commit

--- src/test/java/com/college/CollegeApplicationTests.java
+++ src/test/java/com/college/CollegeApplicationTests.java
@@ -1,13 +1,12 @@
-package com.college;
-
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CollegeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.college;
+ * 
+ * import org.junit.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class CollegeApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
