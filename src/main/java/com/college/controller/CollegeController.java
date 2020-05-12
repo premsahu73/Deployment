6ffe0fd5c85b67b83a0f6e8eaa7d2b485fa679b7
unknown <premprasadsaw73@gmail.com>
2020-05-12 23:34:44 +0530

    first commit

--- src/main/java/com/college/controller/CollegeController.java
+++ src/main/java/com/college/controller/CollegeController.java
@@ -2,8 +2,10 @@ package com.college.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/college")
 public class CollegeController {
 
 	@GetMapping("/get")
